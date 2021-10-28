f3d03052d9a6704b86b63b6d53d8277f9eaf6592
Garett Robson <garett@liquidlight.co.uk>
2021-10-28 12:47:20 +0100

    BE: Simple implementation of seld/jsonlint for linting purposes

--- ci.php
+++ ci.php
@@ -11,6 +11,7 @@ use App\Command\ScssStylelint;
 use App\Command\JsEslint;
 use App\Command\TSLint;
 use App\Command\Run;
+use App\Command\JsonLint;
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -18,6 +19,7 @@ error_reporting(E_ALL);
 
 $application = new Application();
 
+$application->add(new JsonLint(__DIR__));
 $application->add(new PhpCodingStandards(__DIR__));
 $application->add(new ScssStylelint(__DIR__));
 $application->add(new JsEslint(__DIR__));
---
+++ src/Command/JsonLint.php
@@ -0,0 +1,57 @@
+<?php
+
+// src/Command/CreateUserCommand.php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Seld\JsonLint\JsonParser;
+use Exception;
+
+class JsonLint extends Base
+{
+	protected static $defaultName = 'json:lint';
+
+	protected function configure()
+	{
+		$this
+			// the short description shown while running "php bin/console list"
+			->setDescription('JsonLint')
+			->setAliases(['json:lint'])
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+
+		$config = json_decode(file_get_contents($this->path . '/resources/config/jsonlint.json'));
+		$files = $this->findFiles($config->extension, $config->ignore);
+		if (!$files) {
+			return Command::SUCCESS;
+		}
+
+		$this->getTitle();
+
+		$response = Command::SUCCESS;
+		$parser = new JsonParser();
+		foreach($files as $file) {
+			try {
+				$content = $file->getContents();
+				//var_dump($content);
+				$error = $parser->lint($content);
+				if($error) {
+					throw $error;
+				}
+			} catch (Exception $ex) {
+				$this->io->warning("JSON parsing error in " . $file->getRelativePathname());
+				$this->io->text("\t". implode("\n\t", array_map('trim', explode("\n", $ex->getMessage()))));
+				$response = Command::FAILURE;
+			}
+		}
+		return $response;
+	}
+}
