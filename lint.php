233066073f4cffa439e5a20a952c1276be89e118
Mike Street <mike@liquidlight.co.uk>
2022-11-18 09:31:47 +0000

    Chore: Lint PHP

--- lint.php
+++ lint.php
@@ -22,7 +22,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$composerFile = json_decode(file_get_contents(__DIR__ . '/composer.json'));
+$composerFile = json_decode(file_get_contents(__DIR__ . '/composer.json') ?: '');
 
 $application = new Application(
 	$composerFile->description,
